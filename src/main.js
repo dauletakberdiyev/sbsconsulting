48cbfb1c529d944e5fe2e179ee3a65e23a7ff990
HahhVwgwg <serikzhan.parmanov@gmail.com>
2023-06-22 05:28:36 +0600

    work done

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ i18n.global.locale = "ru"
 const app = createApp(App)
 
 app.config.globalProperties.instagramLink = "https://www.instagram.com/step_by_step_consulting/"
-app.config.globalProperties.whatsappLink = "https://wa.me/77474374335"
+app.config.globalProperties.whatsappLink = "https://wa.me/77471804116"
 
 app.use(router)
 app.use(i18n)
